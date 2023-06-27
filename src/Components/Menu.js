69a9113a1b52c73f961588267d3ac02f62566b2c
Capotss <felipebittarmartiniano@gmail.com>
2023-06-27 10:37:33 -0300

    first commit

---
+++ src/Components/Menu.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Menu() {
+    return (
+        <div alt='menu' className=' p-2 flex justify-evenly border-b-2 border-neutral-150'>
+            <button>Categorias ↓</button>
+            <button>The Outsiders Second Hand</button>
+        </div>
+    )
+}
+
+export default Menu
---
+++ src/Components/Nav.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Menu from './Menu'
+function Nav() {
+  return (
+    <nav className='sticky top-0'>
+    <div className=' border-b-2 border-neutral-150 h-20 p-2 flex justify-between items-center'>
+      <a className='h-full ' href='./#' ><img alt='logo' className='h-full ' src='./assets/imgs/logo-to.png'></img></a>
+
+      <input alt='search bar' placeholder='O que você está buscando?' className='bg-neutral-50 inp-nav h-10 border-solid border outline-none border-neutral-600 rounded-md'></input>
+      <div className='flex justify-evenly gap-2'>
+        <button className='btn-nav rounded-md hover:bg-neutral-50'>Minha Conta</button>
+        <button className='btn-nav rounded-md hover:bg-neutral-50'>Contato</button>
+        <button className='btn-nav rounded-md hover:bg-neutral-50'>Meu Carrinho</button>
+      </div>
+    </div>
+    <Menu />
+  </nav>
+  )
+}
+
+export default Nav
